b2d87d57ea44cbd007f47c28163177bb166fed90
Yujun Wu <wyj0912@gmail.com>
2013-04-14 21:30:09 -0700

    s3 credentials

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -1,19 +1,23 @@
-S3_KEY="AKIAJBFJOIWFJZD6AYPA"
-S3_SECRET="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"
 #S3_DEV_BUCKET="testing-item-photos-akiajbfjoiwfjzd6aypa"
 
 S3_STG_BUCKET="staging-item-photos-akiajbfjoiwfjzd6aypa"
-S3_PRO_BUCKET="MuseMe-images-bucket-akiajbfjoiwfjzd6aypa"
-
+S3_PRO_BUCKET="metafad-production"
 S3_DEV_BUCKET="MuseMe-images-bucket-akiajbfjoiwfjzd6aypa"
 
+S3_PRO_KEY="AKIAJI5MZU6KL6UUYPSQ"
+S3_PRO_SECRET="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"
 
-if Rails.env.development?
-	bucket = S3_DEV_BUCKET
-elsif Rails.env.production?
+S3_DEV_KEY="AKIAJBFJOIWFJZD6AYPA"
+S3_DEV_SECRET="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"
+	
+if Rails.env.production?
 	bucket = S3_PRO_BUCKET
+	S3_KEY = S3_PRO_KEY
+    S3_SECRET = S3_PRO_SECRET
 else
-	bucket = S3_STG_BUCKET
+	bucket = S3_DEV_BUCKET
+	S3_KEY = S3_DEV_KEY
+    S3_SECRET = S3_DEV_SECRET
 end
 
 S3_CREDENTIALS = {
