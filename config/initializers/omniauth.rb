5169c162882689a6f18bbacc720862023beccbca
Yujun Wu <wyj0912@gmail.com>
2013-04-25 23:06:49 -0700

    fb log in debug

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,12 +1,14 @@
-if Rails.env == "development"
-	Rails.application.config.middleware.use OmniAuth::Builder do
-      provider :facebook, '116501321837174', 'd9c705735f7d8fc99ff9a74384072668',  
-      :scope => "publish_actions,email", :image_size => 'normal'
-                
-    end
+if Rails.env.production?
+	FB_ID = '142566439218323'
+	FB_SECRET = 'a08795d99fc0fbf0817fb13c700a50fe'
+	DOMAIN = 'www.metafad.com'
 else
-	Rails.application.config.middleware.use OmniAuth::Builder do
-	  provider :facebook, '142566439218323','a08795d99fc0fbf0817fb13c700a50fe',
-	  :scope => "publish_actions,email",:image_size => 'normal'
-	end
+	FB_ID = '116501321837174'
+	FB_SECRET = 'd9c705735f7d8fc99ff9a74384072668'
+	DOMAIN = 'localhost:3000'
+end
+
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :facebook, FB_ID, FB_SECRET,
+  :scope => "publish_actions,email", :image_size => 'normal'
 end
